848ed4e2b2943958eac273793c70cc6f88f18909
Tuaro <mail.o.litvinov@gmail.com>
2017-03-20 16:36:37 +0200

    20.03.2017

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1408/Hen.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task14.task1408;
+
+/**
+ * Created by oleg on 20.03.2017.
+ */
+public abstract class Hen {
+
+   public String getDescription()
+   {
+     return "Я - курица";
+   }
+
+   public abstract int getCountOfEggsPerMonth();
+
+                            }
+
+
